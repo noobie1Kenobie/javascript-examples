55a961ce64dec794efddc321d24154bd23c45395
noobie1Kenobie <rxmowe@hotmail.com>
2025-07-11 03:32:05 +0800

    Coded the right solutions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// If you ever find that 'document.querySelector(".classname")'
+// returns a "null" then try the following:
+//   1) put the script block beneath the body for html
+//   2) put the reference for the js script after the body block in the html.
+
 const people = [
   "Chris",
   "Anne",
@@ -15,7 +20,19 @@ const refused = document.querySelector(".refused");
 admitted.textContent = "Admit: ";
 refused.textContent = "Refuse: ";
 
+let refusedList = [];
+let admitList = [];
 // loop starts here
+for (person of people) {
+  console.log(person);
+  if (person == "Phil" || person == "Lola") {
+    refusedList.push(person);
+  } else {
+    admitList.push(person);
+  }
+}
+console.log("Refused: " + refusedList);
+console.log("Admit: " + admitList);
 
-// refused.textContent += ;
-// admitted.textContent += ;
+refused.textContent += refusedList;
+admitted.textContent += admitList;
